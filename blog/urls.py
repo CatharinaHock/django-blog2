ffaf4f34402aefc96c234440c275aa51b840adf1
chock <catharina_lan.hock@gmx.de>
2025-01-12 20:57:33 -0500

    Added view and template for detailed blog post

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views # views from this folder, hence the dot.
 
 urlpatterns = [
     path('', views.post_list, name="post_list"),
+    path("post/<int:pk>/", views.post_detail, name="post_detail"),
 ]
