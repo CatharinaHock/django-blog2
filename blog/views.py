7f24e6c35bb8a2ed7cb1491281c558c71779a1af
chock <catharina_lan.hock@gmx.de>
2025-01-15 17:33:12 -0500

    Added more security and Login page.

--- blog/views.py
+++ blog/views.py
@@ -3,20 +3,24 @@ from .models import Post
 from django.utils import timezone
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def post_list(request):
     posts=Post.objects.filter(published_date__lte=timezone.now()).order_by("-published_date")
     return render(request, 'blog/post_list.html', {"posts":posts}) # referenz zu blog/templates/blog/post_list.html
 
+@login_required
 def post_draft_list(request):
     posts=Post.objects.filter(published_date__isnull=True).order_by("created_date")
     return render(request, 'blog/post_draft_list.html', {"posts":posts}) # referenz zu blog/templates/blog/post_list.html
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk = pk)
     return render(request, "blog/post_detail.html", {"post":post})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -30,6 +34,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -47,12 +52,14 @@ def post_edit(request, pk):
 def about(request):
     return render(request, "blog/about.html", {})
 
+@login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method=='POST':
         post.publish()
     return redirect('post_detail', pk=pk)
 
+@login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method=='POST':
