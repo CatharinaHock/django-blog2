71c0ef188f0c60c6e94fbf744caf3558195678ba
chock <catharina_lan.hock@gmx.de>
2025-01-24 23:05:21 -0500

    added tags

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,15 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from colorfield.fields import ColorField
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    background_color = ColorField(default='#000000')
+    text_color = ColorField(default='#FFFFFF')
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -12,6 +20,7 @@ class Post(models.Model):
     published_date = models.DateTimeField(blank=True, null=True)
     main_img = models.ImageField(upload_to='images/', default="images/default.png")
     color_rotation = models.IntegerField(default=0)
+    tags = models.ManyToManyField(Tag)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -21,6 +30,8 @@ class Post(models.Model):
         return self.title
     def approved_comments(self):
         return self.comments.filter(approved_comment=True)
+
+
     
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
